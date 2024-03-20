767e6472ffe16ec0f5c241d6b39fa7bfe63c5fe7
ryanaswift7 <ryanaswift7@gmail.com>
2024-03-19 23:25:40 -0700

    refactored BoundaryLineObject into abstract class

--- src/FurnitureObjects/Essentials/Wall.java
+++ src/FurnitureObjects/Essentials/Wall.java
@@ -5,104 +5,29 @@ import Templates.FurnitureObject;
 import Templates.Movable;
 
 import java.awt.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class Wall implements BoundaryLineObject, Movable {
-    private int x1, y1, x2, y2, deltaX, deltaY;
+public class Wall extends BoundaryLineObject {
     private static final String name = "Wall";
+    private static final BasicStroke stroke = new BasicStroke(4);
+    private static final Color color = Color.BLACK;
 
     public Wall(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+        super(x1, y1, x2, y2);
+        setName(name);
+        setColor(color);
+        setStroke(stroke);
     }
     public Wall() {
-        this.x1 = 25;
-        this.y1 = 0;
-        this.x2 = 25;
-        this.y2 = 50;
+        super();
+        setName(name);
+        setColor(color);
+        setStroke(stroke);
     }
-    public void loadImage() { return;}
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void draw(Graphics2D g) {
-        Stroke defaultStroke = g.getStroke(); // Save the default stroke
-
-        // Set the thickness of the wall
-        g.setStroke(new BasicStroke(3)); // Thick black line
-
-        // Set the color of the wall
-        g.setColor(Color.BLACK);
-
-        // make sure lines are drawn perfectly horizontal or vertical
-        int diff_x = Math.abs(x2-x1);
-        int diff_y = Math.abs(y2-y1);
-        if (diff_x <= diff_y){ x2 = x1; }
-        else { y2 = y1; }
-
-
-        snapToGrid();
-        // Draw the wall
-        g.drawLine(x1, y1, x2, y2);
-        g.setStroke(defaultStroke); // Reset stroke to default
-
-    }
-
     @Override
     public FurnitureObject createObjectAtPosition(Point position) {
         return new Wall(position.x, position.y, position.x, position.y + 5);
-    }
-    @Override
-    public void updateEndpoint(Point point) {
-        x2 = point.x;
-        y2 = point.y;
-    }
-    @Override
-    public Rectangle getBoundingBox() {
-        if (y1 == y2) {
-            return new Rectangle(x1, y1-2, x2-x1, 4);
-        }
-        else { // x1 == x2
-            return new Rectangle(x1-2, y1, 4, y2-y1);
-        }
-    }
-    @Override
-    public void snapToGrid() {
-        int gridSize = 20;
-        x1 = (int) (Math.round(x1 / (double) gridSize) * gridSize);
-        y1 = (int) (Math.round(y1 / (double) gridSize) * gridSize);
-        x2 = (int) (Math.round(x2 / (double) gridSize) * gridSize);
-        y2 = (int) (Math.round(y2 / (double) gridSize) * gridSize);
-    }
-
-    @Override
-    public Point getPosition() {
-        deltaX = x2 - x1;
-        deltaY = y2 - y1;
-        return new Point(x1, y1);
-    }
-
-    @Override
-    public void setPosition(Point newPosition) {
-        x1 = newPosition.x;
-        y1 = newPosition.y;
-        x2 = x1 + deltaX;
-        y2 = y1 + deltaY;
-    }
-    @Override
-    public void writeObject(ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject(); // Write other fields
-        out.writeInt(0); // Indicate that no image is available
-    }
-
-    @Override
-    // Custom deserialization logic for transient field doorImage
-    public void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject(); // Read other fields, ignore looking for image
     }
 }
--- src/Templates/BoundaryLineObject.java
+++ src/Templates/BoundaryLineObject.java
@@ -2,7 +2,108 @@ package Templates;
 
 import java.awt.*;
 
-public interface BoundaryLineObject extends FurnitureObject {
-    void snapToGrid();
-    void updateEndpoint(Point point);
+import FurnitureObjects.Essentials.Window;
+import Templates.FurnitureObject;
+import Templates.Movable;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public abstract class BoundaryLineObject implements FurnitureObject, Movable {
+    protected int x1, y1, x2, y2, deltaX, deltaY;
+    protected String name;
+    protected Color color;
+    protected BasicStroke stroke;
+
+    public BoundaryLineObject(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public BoundaryLineObject() {
+        this.x1 = 25;
+        this.y1 = 0;
+        this.x2 = 25;
+        this.y2 = 50;
+    }
+    protected void setName(String name) { this.name = name; }
+    protected void setColor(Color color) { this.color = color; }
+    protected void setStroke(BasicStroke stroke) { this.stroke = stroke; }
+    public void loadImage() { return;}
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        Stroke defaultStroke = g.getStroke(); // Save the default stroke
+
+        // Set the thickness
+        g.setStroke(stroke);
+
+        g.setColor(color);
+
+        // make sure lines are drawn perfectly horizontal or vertical
+        int diff_x = Math.abs(x2-x1);
+        int diff_y = Math.abs(y2-y1);
+        if (diff_x <= diff_y){ x2 = x1; }
+        else { y2 = y1; }
+
+
+        snapToGrid();
+        g.drawLine(x1, y1, x2, y2);
+        g.setStroke(defaultStroke); // Reset stroke to default
+
+    }
+
+    public void updateEndpoint(Point point) {
+        x2 = point.x;
+        y2 = point.y;
+    }
+    @Override
+    public Rectangle getBoundingBox() {
+        if (y1 == y2) {
+            return new Rectangle(x1, y1-2, x2-x1, 4);
+        }
+        else { // x1 == x2
+            return new Rectangle(x1-2, y1, 4, y2-y1);
+        }
+    }
+
+    public void snapToGrid() {
+        int gridSize = 20;
+        x1 = (int) (Math.round(x1 / (double) gridSize) * gridSize);
+        y1 = (int) (Math.round(y1 / (double) gridSize) * gridSize);
+        x2 = (int) (Math.round(x2 / (double) gridSize) * gridSize);
+        y2 = (int) (Math.round(y2 / (double) gridSize) * gridSize);
+    }
+
+    @Override
+    public Point getPosition() {
+        deltaX = x2 - x1;
+        deltaY = y2 - y1;
+        return new Point(x1, y1);
+    }
+
+    @Override
+    public void setPosition(Point newPosition) {
+        x1 = newPosition.x;
+        y1 = newPosition.y;
+        x2 = x1 + deltaX;
+        y2 = y1 + deltaY;
+    }
+    @Override
+    public void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject(); // Write other fields
+        out.writeInt(0); // Indicate that no image is available
+    }
+
+    @Override
+    // Custom deserialization logic for transient field doorImage
+    public void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject(); // Read other fields, ignore looking for image
+    }
 }
